e5d1c43659f066ef4a46077bb3ea10d69cb1ff05
AndresEzio <andres_ezio@hotmail.com>
2018-11-27 07:49:07 -0600

    'actividad_8'

--- app/src/main/java/unam/fca/dmoviles/MainActivity.java
+++ app/src/main/java/unam/fca/dmoviles/MainActivity.java
@@ -2,6 +2,10 @@ package unam.fca.dmoviles;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +23,27 @@ public class MainActivity extends AppCompatActivity {
         // res/layout/activity_main.xml
         setContentView(R.layout.activity_main);
 
+        //Se hace referencia al boton dentro del layout
+        final Button btn = findViewById(R.id.button);
+
+       //Se hace referencia al componente editText del layout
+        final EditText et1 = (EditText)findViewById(R.id.editText);
+
+        //Se hace referencia al componente editText1 del layout
+        final EditText et2 = (EditText)findViewById(R.id.editText2);
+
+        //Se hace referencia al componente textViewResult del layout
+        final TextView tv = findViewById(R.id.textViewResult);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                float v1 = Float.parseFloat(String.valueOf(et1.getText()));
+                float v2 = Float.parseFloat(String.valueOf(et2.getText()));
+                float difRelativa = (v2-v1) * 100 / Math.abs(v1);
+                tv.setText(difRelativa+"");
+            }
+        });
+
 
 
         //Actividades
@@ -33,5 +58,27 @@ public class MainActivity extends AppCompatActivity {
         //g) Sube el documento Word a la plataforma Moodle. Incluye la liga a tu repositorio
 
 
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        final TextView tv = findViewById(R.id.textViewResult);
+        final EditText et1 = (EditText) findViewById(R.id.editText);
+        final EditText et2 = (EditText) findViewById(R.id.editText2);
+        tv.setText(savedInstanceState.getString("resultado"));
+        et1.setText(savedInstanceState.getString("valor1"));
+        et2.setText(savedInstanceState.getString("valor2"));
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        final TextView tv = findViewById(R.id.textViewResult);
+        final EditText et1 = (EditText) findViewById(R.id.editText);
+        final EditText et2 = (EditText) findViewById(R.id.editText2);
+        outState.putString("resultado", tv.getText().toString());
+        outState.putString("valor1",et1.getText().toString());
+        outState.putString("valor2",et2.getText().toString());
+        super.onSaveInstanceState(outState);
     }
 }
